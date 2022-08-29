d1f150b93cb7f4d4b1ab0f4879029b7737bcc52c
Langer,Christoph <christoph.langer@imba.oeaw.ac.at>
2022-08-29 15:24:32 +0200

    Update README and make paths relative as default

--- submit_snakemake_job.sh
+++ submit_snakemake_job.sh
@@ -1,2 +1,2 @@
 snakemake --use-conda --use-singularity --rerun-incomplete --cluster "sbatch -t {cluster.time} -c {cluster.nodes} --mem {cluster.memory}"\
-          --cluster-config /groups/gerlich/experiments/Experiments_005400/005484/sequencing_analysis/sister-pore-c-snakemake/config/cluster_config.yml --jobs 300 --output-wait 60
+          --cluster-config config/cluster_config.yml --jobs 300 --output-wait 60
